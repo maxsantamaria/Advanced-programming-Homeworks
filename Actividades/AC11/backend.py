a76249f16ee257d712e68ec63c4d054e11c8f70a
MaxSantamaria <mesantamaria@uc.cl>
2017-10-19 16:43:24 -0300

    2

--- Actividades/AC11/backend.py
+++ Actividades/AC11/backend.py
@@ -3,50 +3,85 @@ import os.path
 
 class Usuario:
 
-	def __init__(self, nombre, valor_ultima_apuesta, valor_maxima_apuesta, saldo):
-		self.nombre = nombre
-		self._valor_ultima_apuesta = valor_ultima_apuesta
-		self._valor_maxima_apuesta = valor_maxima_apuesta
-		self._saldo = saldo
+    def __init__(self, nombre, valor_ultima_apuesta, valor_maxima_apuesta, saldo):
+        self.nombre = nombre
+        self._valor_ultima_apuesta = valor_ultima_apuesta
+        self._valor_maxima_apuesta = valor_maxima_apuesta
+        self._saldo = saldo
 
-	@property
-	def saldo(self):
-		return self._saldo
-	
-	@saldo.setter
-	def saldo(self, value):
-		self._saldo = value
+    @property
+    def saldo(self):
+        return self._saldo
 
-	@property
-	def valor_ultima_apuesta(self):
-		return self._valor_ultima_apuesta
-	
-	@valor_ultima_apuesta.setter
-	def valor_ultima_apuesta(self, value):
-		self._valor_ultima_apuesta = value
+    @saldo.setter
+    def saldo(self, value):
+        self._saldo = value
 
+    @property
+    def valor_ultima_apuesta(self):
+        return self._valor_ultima_apuesta
 
-	@property
-	def valor_maxima_apuesta(self):
-		return self._valor_maxima_apuesta
-	
-	@valor_maxima_apuesta.setter
-	def valor_maxima_apuesta(self, value):
-		self._valor_maxima_apuesta = value
+    @valor_ultima_apuesta.setter
+    def valor_ultima_apuesta(self, value):
+        self._valor_ultima_apuesta = value
 
 
-	def datos_para_archivo(self):
-		return "{}|{}|{}|{}\n".format(self.nombre, self.valor_ultima_apuesta, self.valor_maxima_apuesta, self.saldo)
+    @property
+    def valor_maxima_apuesta(self):
+        return self._valor_maxima_apuesta
+
+    @valor_maxima_apuesta.setter
+    def valor_maxima_apuesta(self, value):
+        self._valor_maxima_apuesta = value
+
+
+    def datos_para_archivo(self):
+        return "{}|{}|{}|{}\n".format(self.nombre, self.valor_ultima_apuesta, self.valor_maxima_apuesta, self.saldo)
+
+
+
+    def cambiar_apuesta_maxima(self, apuesta):  # apuesta en str
+        apuesta_int = int(apuesta)
+        if apuesta_int > self.valor_maxima_apuesta:
+            self.valor_maxima_apuesta = apuesta_int
+
+
+    def __repr__(self):
+        return "{}|{}|{}|{}\n".format(self.nombre, self.valor_ultima_apuesta,
+                                      self.valor_maxima_apuesta, self.saldo)
 
 
 def verificar_usuario(nombre):
-	existencia = os.path.exists("usuarios.txt")
-	if not existencia:
-		with open("usuarios.txt", "w") as file:
-			usuario = Usuario(nombre, 0, 0, 1500)
-			file.write(usuario.datos_para_archivo())
+    existencia = os.path.exists("usuarios.txt")
+    if not existencia:
+        with open("usuarios.txt", "w") as file:
+            usuario = Usuario(nombre, 0, 0, 1500)
+            file.write(usuario.datos_para_archivo())
 
-	elif existencia:
-		with open("usuarios.txt", "r") as file:
-			datos_usuario = list(filter(lambda x: x.split("|")[0] == nombre, [linea for linea in file]))
+    elif existencia:
+        print("a")
+        with open("usuarios.txt", "r") as file:
+            datos_usuario = list(filter(lambda x: x.strip().split("|")[0] == nombre, [linea for linea in file]))
+            print(datos_usuario)
+            datos_usuario = datos_usuario[0].strip().split("|")
+            print(datos_usuario)
+            usuario = Usuario(*datos_usuario)
+    return usuario
+
+
+def guardar_informacion(usuario):
+	esplitear = lambda x: x.split("|")
+	with open("usuarios.txt", "r") as file:
+		usuarios_lista = [esplitear(linea) for linea in file]	
+		nuevos_datos_usuario = [usuario.nombre, usuario.valor_ultima_apuesta, usuario.valor_maxima_apuesta, usuario.saldo]
+	for i in range(len(usuarios_lista)):
+		if usuarios_lista[i][0] == nuevos_datos_usuario[0]:
+			indice = i
+	usuarios_lista[i] = nuevos_datos_usuario
+	with open("usuarios.txt", "w") as file:
+		file.write(usuario_x.join("|") for usuario_x in usuarios_lista)
+
+
+
+
 
--- Actividades/AC11/main.py
+++ Actividades/AC11/main.py
@@ -1,5 +1,5 @@
 import sys
-import backend
+from backend import verificar_usuario
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 
@@ -13,17 +13,24 @@ class Casino(formulario[0], formulario[1]):
     def __init__(self, nombre):
         super().__init__()
         self.setupUi(self)
-        #self.BotonAumentar.clicke
         self.nombre = nombre
-        self.usuario = backend.verificar_usuario(nombre)
-        self.saludo.setText("Hola " + self.usuario.nombre)
-        self.apuesta_actual.setText(str(self.usuario.saldo))
-        self.maximo_premio
+        #self.usuario = verificar_usuario(nombre)
+        self.saludo.setText("Hola " + self.nombre)
+        #self.apuesta_actual.setText(str(self.usuario.saldo))
+        #self.maximo_premio.setText(str(self.usuario.valor_maxima_apuesta))
+        #self.ultima_apuesta.setText(str(self.usuario._valor_ultima_apuesta))
+        #self.apuesta = 500
+        self.saldo_actual.setText(str(1500))
+        self.maximo_premio.setText(str(0))
+        self.ultima_apuesta.setText(str(0))
 
+        self.apuesta_actual.setText("500")
+        self.up.clicked.connect(self.aumentar)
+        #self.down.clicked.connect(self.disminuir)
 
-
-
-
+    def aumentar(self):
+        self.apuesta += 1
+        self.apuesta_actual.setText(str(self.apuesta))
 
 
 class Register(ingreso[0], ingreso[1]):
